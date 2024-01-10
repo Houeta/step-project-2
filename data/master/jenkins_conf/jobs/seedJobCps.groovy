d1c9e5a3714d7f148214c263b21ae726caefb758
pathetic. <miracle1695@gmail.com>
2024-01-10 21:08:12 +0200

    I won ?

--- data/master/jenkins_conf/jobs/seedJobCps.groovy
+++ data/master/jenkins_conf/jobs/seedJobCps.groovy
@@ -1,10 +1,59 @@
-
+echo "${Jenkins.instance.getJob('js-pipeline-cps').workspace}/my-pipeline.groovy"
 
 pipelineJob('js-pipeline-cps') {
     definition {
         cps {
-            script(readFileFromWorkspace("${Jenkins.instance.getJob('js-pipeline-cps').workspace}/my-pipeline.groovy"))
+            script('''
+                pipeline {
+                    agent {
+                        label 'docker'
+                    }
 
+                    environment {
+                        github_url = 'https://github.com/Houeta/step-project-2'
+                        major_v = 1
+                        registry = 'pathetic/step-project'
+
+                        DOCKER_IMAGE = "${registry}:${major_v}.${env.BUILD_NUMBER}"
+                        DOCKER_CREDS = credentials('docker-creds')
+                    }
+
+                    stages {
+                        stage('Checkout') {
+                            steps {
+                                git branch: 'main', url: github_url
+                            }
+                        }
+
+                        stage('Build image') {
+                            steps {
+                                echo "Build image with tag v${major_v}.${env.BUILD_NUMBER}"
+                                sh "docker build -t ${DOCKER_IMAGE} ."
+                            }
+                        }
+
+                        stage('Test') {
+                            steps {
+                                echo 'Run tests based on jtest'
+                                sh "docker run ${DOCKER_IMAGE} test"
+                            }
+                        }
+                    }
+
+                    post {
+                        success {
+                            sh """
+                            docker login -u ${DOCKER_CREDS_USR} -p ${DOCKER_CREDS_PSW}
+                            docker push ${DOCKER_IMAGE}
+                            """
+                        }
+                        failure {
+                            echo 'Tests failed'
+                        }
+                    }
+
+                }
+            ''')
         }
     }
     triggers {
