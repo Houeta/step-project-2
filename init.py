196e81925a96f4566aed0941d78d3b05b7f7aa11
miracle <miracle1695@gmail.com>
2024-01-13 15:15:53 +0200

    make init.py most pretty

--- init.py
+++ init.py
@@ -9,7 +9,8 @@ from os.path import join, dirname
 import validators
 
 env_file = '.env.jenkins'
-dotenv_path = join(dirname(__file__), 'data', 'master', 'jenkins_conf', env_file)
+dotenv_path = join(dirname(__file__), 'data',
+                   'master', 'jenkins_conf', env_file)
 list_of_ENV = {
     'ADMIN_USER': ["Enter a username for Jenkins", "admin"],
     'ADMIN_PSW': ["Enter a password for Jenkins", "admin"],
@@ -17,7 +18,8 @@ list_of_ENV = {
     'GITHUB_URL': ["Enter an GitHub URL with your repo", "https://github.com/Houeta/step-project-2"],
     'DOCKER_USR': ["Enter your docker username"],
     'DOCKER_PSW': ["Enter your docker password"],
-    }
+}
+
 
 def wrap_input(env, env_value):
     try:
@@ -27,22 +29,19 @@ def wrap_input(env, env_value):
 
     if '_PSW' in env:
         user_value = getpass(prompt=output_string)
-    elif '_EMAIL' in env:
-        user_value = input(output_string)
-        if user_value and not validate_email_syntax(user_value):
-            wrap_input(env, env_value)
-    elif '_URL' in env:
-        user_value = input(output_string)
-        if user_value and not validators.url(user_value):
-            wrap_input(env, env_value)
     else:
         user_value = input(output_string)
+        if '_EMAIL' in env and user_value and not validate_email_syntax(
+                user_value) or '_URL' in env and user_value and not validators.url(
+                user_value):
+            return wrap_input(env, env_value)
     try:
         if not user_value:
             user_value = env_value[1]
     except Exception as e:
         user_value = ''
     return user_value
+
 
 def set_default_value(env, env_value):
     try:
@@ -51,9 +50,11 @@ def set_default_value(env, env_value):
         default_value = ''
     set_variable_in_file(env, default_value)
 
+
 def set_variable_in_file(key, value, env_file=dotenv_path):
     environ[key] = value
     set_key(env_file, key, environ[key], quote_mode="never")
+
 
 def validate_email_syntax(email):
     if not email:
@@ -68,9 +69,9 @@ if __name__ == '__main__':
 
     try:
         for env_key, env_value in list_of_ENV.items():
-            set_variable_in_file(env_key, wrap_input(env_key, env_value))
+            valide_value = wrap_input(env_key, env_value)
+            set_variable_in_file(env_key, valide_value)
     except Exception as e:
         print(e)
         for env_key, env_value in list_of_ENV.items():
             set_default_value(env_key, env_value)
-            
